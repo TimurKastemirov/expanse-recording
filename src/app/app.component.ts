c4e59c82ac6634315c3ee0fafa0f1141fe76b8b8
TimKast <tamerlan8.05.92@gmail.com>
2018-10-05 23:41:11 +0300

    expanse categories start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {setTheme} from 'ngx-bootstrap/utils';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Expanse Recording';
+    title = 'Expanse Recording';
+    constructor() {
+        setTheme('bs4'); // or 'bs4'
+    }
 }
